cac6df84b38bf94e14df872b0dd1ea7aaa1b23a5
Evgeniy <myiphone128@mail.ru>
2024-06-18 16:58:45 +0300

    fixed bugs, added popup with contacts

--- prerelease/scripts.js
+++ prerelease/scripts.js
@@ -9,6 +9,21 @@ function checkFade() {
     }
   });
 }
-
 window.addEventListener("scroll", checkFade);
 window.addEventListener("load", checkFade);
+
+const navLinkToPortfolio = document.querySelector(".nav__link-portfolio");
+const portfolioIntro = document.querySelector(".portfolio__intro");
+navLinkToPortfolio.addEventListener("click", () => {
+  portfolioIntro.scrollIntoView({ behavior: "smooth", block: "start" });
+});
+
+const popup = document.querySelector(".popup");
+const popupOverlay = document.querySelector(".popup__overlay");
+const popupMessage = document.querySelector(".popup__message");
+popup.addEventListener("click", (event) => {
+  if (event.target.classList.contains("popup__toggle")) {
+    popupOverlay.classList.toggle("active");
+    popupMessage.classList.toggle("active");
+  }
+});
