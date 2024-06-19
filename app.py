9a8bdd5026b9f8132b3bcfc4453dd8efe8b4e297
FiX <myiphone128@mail.ru>
2024-06-19 21:38:10 +0300

    async flask

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, send_from_directory
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+async def index():
+    return send_from_directory('templates', 'index.html')
 
 
 if __name__ == '__main__':
