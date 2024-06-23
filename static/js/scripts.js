55d0da3c88d41e745416d638f7358ad3d57f577d
FiX <myiphone128@mail.ru>
2024-06-23 15:18:10 +0300

    fixes, optimization

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -29,6 +29,10 @@ const discountDays = document.querySelector(".discounts__time-days");
 const discountHours = document.querySelector(".discounts__time-hours");
 const discountMinutes = document.querySelector(".discounts__time-minutes");
 const discountSeconds = document.querySelector(".discounts__time-seconds");
+const discountPercent = document.querySelector(".discounts__progressbar-title");
+const discountProgresBar = document.querySelector(
+  ".discounts__progressbar-done"
+);
 function updateTextContent(element, value) {
   if (element.textContent != value) {
     element.textContent = value;
@@ -37,17 +41,22 @@ function updateTextContent(element, value) {
 
 function changeTime() {
   const dateNow = new Date();
+  const discountStart = new Date(2024, 5, 20);
   const discountsEnd = new Date(2024, 6, 6);
   const diff = discountsEnd - dateNow;
+  const percents = Math.floor(diff / ((discountsEnd - discountStart) / 100));
 
   updateTextContent(discountDays, Math.floor(diff / (1000 * 60 * 60 * 24)));
   updateTextContent(discountHours, Math.floor((diff / (1000 * 60 * 60)) % 24));
   updateTextContent(discountMinutes, Math.floor((diff / (1000 * 60)) % 60));
   updateTextContent(discountSeconds, Math.floor((diff / 1000) % 60));
+  updateTextContent(discountPercent, percents);
+
+  discountProgresBar.style.width = `${percents}%`;
 
   setTimeout(changeTime, 1000);
 }
-setTimeout(changeTime, 1000);
+setTimeout(changeTime, 300);
 
 const popupButtonOutside = document.querySelector(".popup__button-outside");
 const popupButtonInside = document.querySelector(".popup__button-inside");
