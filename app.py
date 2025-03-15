9df3b39ca58287baa7726e5c2ba48e0d319b5add
Evgeniy <myiphone128@mail.ru>
2025-03-15 17:52:24 +0200

    mobile adaptation, styles, translations update

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ app.config['BABEL_TRANSLATION_DIRECTORIES'] = 'translations'
 def get_locale():
     lang = request.args.get('lang')
     if not lang:
-        lang = request.accept_languages.best_match(['ru', 'uk', 'en'])
+        lang = request.accept_languages.best_match(['uk', 'en'])
 
     return lang
 
